fac6aef9856668996aa954c151da533a7fbd7f52
Oussema Naffety <oussemanaffety@sfax.r-iset.tn>
2023-10-16 20:36:17 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExempleComponent } from './exemple/exemple.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExempleComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/exemple/exemple.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exemple',
+  template: `
+    <p>
+      exemple works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ExempleComponent {
+
+}
